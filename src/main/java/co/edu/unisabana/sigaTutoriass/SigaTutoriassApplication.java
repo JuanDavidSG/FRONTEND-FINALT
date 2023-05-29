a2c239adc416ccacc5f81b0dc0a5cd0bb3c7637f
juan david sanchez <Juansangon@unisabana.edu.co>
2023-05-29 01:33:12 -0500

    Se hace la respectiva identacion del codigo

--- src/main/java/co/edu/unisabana/sigaTutoriass/SigaTutoriassApplication.java
+++ src/main/java/co/edu/unisabana/sigaTutoriass/SigaTutoriassApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SigaTutoriassApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SigaTutoriassApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SigaTutoriassApplication.class, args);
+    }
 
 }
